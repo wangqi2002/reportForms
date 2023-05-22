fd6dd1bd57917349f8407b4ae515c4f7038f97c7
wangqi <1722009706@qq.com>
2023-05-22 09:55:36 +0800

    项目初始化`

--- reportforms/src/views/HomeView.vue
+++ reportforms/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Luckysheet></Luckysheet>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Luckysheet from "@/components/Luckysheet.vue";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    Luckysheet,
+  },
+};
 </script>
