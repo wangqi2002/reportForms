ed29644f5d71f026af8260b22d6eb5e288793019
f2w00 <109409603+f2w00@users.noreply.github.com>
2023-05-28 19:42:47 +0800

    增加striper

--- date.js
+++ date.js
@@ -4,11 +4,7 @@ let patternTemplate = {
     thisYear: '^${params[0]}',
 } //注意正则表达式中-需要放到边界,否则会被误会为范围
 
-let currentDate = new Date(options.date ? options.date : null)
-let year = currentDate.getFullYear()
-let month = currentDate.getMonth() + 1
-let day = currentDate.getDate()
-let pattern = new RegExp(generatePattern(patternTemplate.today, [year, month, day]))
+let map = new Map()
 
 /**
  * @param {string} raw
@@ -27,20 +23,29 @@ function generatePattern(raw, params) {
  * - `thisYear` 今年
  * - `range` 日期范围
  * @param {{start:Date|string,end:Date|string}} [range]
+ * @example
+ * let filter = configureFilter('today')
+ * filter('2023/5/28')
  * @returns
  */
-function configurePattern(purpose, range, options) {
+function configureFilter(purpose, range, date) {
+    let currentDate = date ? new Date(date) : new Date()
+    let pattern = undefined
     switch (purpose) {
         case 'today':
-            pattern = new RegExp(generatePattern(patternTemplate.today, [year, month, day]))
+            pattern = new RegExp(
+                generatePattern(patternTemplate.today, [
+                    currentDate.getFullYear(),
+                    currentDate.getMonth() + 1,
+                    currentDate.getDate(),
+                ])
+            )
             break
         case 'thisWeek':
             {
                 //并非使用正则表达式,而是替换了pattern.test方法
                 const oneDayTime = 1000 * 60 * 60 * 24
-                const week = parseInt(
-                    (parseInt(new Date(options.date ? options.date : null).getTime() / oneDayTime) + 4) / 7
-                )
+                const week = parseInt((parseInt(currentDate.getTime() / oneDayTime) + 4) / 7)
                 pattern.test = (x) => {
                     let xDate = parseInt(new Date(x).getTime() / oneDayTime)
                     return parseInt((xDate + 4) / 7) == week
@@ -48,10 +53,12 @@ function configurePattern(purpose, range, options) {
             }
             break
         case 'thisMonth':
-            pattern = new RegExp(generatePattern(patternTemplate.thisMonth, [year, month]))
+            pattern = new RegExp(
+                generatePattern(patternTemplate.thisMonth, [currentDate.getFullYear(), currentDate.getMonth() + 1])
+            )
             break
         case 'thisYear':
-            pattern = new RegExp(generatePattern(patternTemplate.thisYear, [year]))
+            pattern = new RegExp(generatePattern(patternTemplate.thisYear, [currentDate.getFullYear()]))
             break
         case 'range': {
             if (range) {
@@ -71,19 +78,85 @@ function configurePattern(purpose, range, options) {
         default:
             return false
     }
-    return true
+    let filter = (x) => pattern.test(x)
+    return filter
 }
 
-function filter(value) {
-    return pattern.test(value)
+function configureStriper(purpose, dateStart, dateEnd) {
+    let striper = undefined
+    let start = new Date(dateStart)
+    let end = new Date(dateEnd)
+
+    switch (purpose) {
+        case 'byDay':
+            {
+                let map = new Map()
+                let counter = 0
+                striper = (x) => {
+                    let dateX = new Date(x)
+                    if (dateX <= end && dateX >= start) {
+                        let current = map.get(dateX.toLocaleDateString())
+                        if (current || current == 0) {
+                            return current
+                        } else {
+                            map.set(dateX.toLocaleDateString(), counter)
+                            return counter++
+                        }
+                    } else {
+                        return false
+                    }
+                }
+            }
+            break
+        case 'byMonth':
+            {
+                let map = new Map()
+                let counter = 0
+                striper = (x) => {
+                    let dateX = new Date(x)
+                    if (dateX <= end && dateX >= start) {
+                        let current = map.get(dateX.getFullYear().toString() + '/' + dateX.getMonth().toString())
+                        if (current || current == 0) {
+                            return current
+                        } else {
+                            map.set(dateX.getFullYear().toString() + '/' + dateX.getMonth().toString(), counter)
+                            return counter++
+                        }
+                    } else {
+                        return false
+                    }
+                }
+            }
+            break
+        case 'byYear':
+            {
+                let map = new Map()
+                let counter = 0
+                striper = (x) => {
+                    let dateX = new Date(x)
+                    if (dateX <= end && dateX >= start) {
+                        let current = map.get(dateX.getFullYear())
+                        if (current || current == 0) {
+                            return current
+                        } else {
+                            map.set(dateX.getFullYear(), counter)
+                            return counter++
+                        }
+                    } else {
+                        return false
+                    }
+                }
+            }
+            break
+
+        default:
+            break
+    }
+    return striper
 }
 
-// changeCurrentPattern()
-
-// // console.log(pattern)
-// // changeCurrentPattern('range', { start: new Date('2022/5/1'), end: new Date('2023-4-20') })
-// // console.log(pattern)
-// // console.log(filter('2023 05 27'))
-// // console.log(getRangeString('2017', '2022'))
-// changeCurrentPattern('thisWeek')
-// console.log(filter('2023 5 22'))
+// let striper = configureStriper('byDay', '2022/4/1', '2023/5/26')
+// let a = ['2023/4/1', '2023/5/26', '2023/4/2', '2023/5/2', '2021/4/8', '2023/4/1', '2023/4/2', '2022/4/2']
+// a.forEach((x) => {
+//     console.log(striper(x))
+// })
